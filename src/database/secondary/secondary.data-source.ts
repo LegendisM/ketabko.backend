063ec9550f988d39cca262513b57af93945ae8c8
LegendisM <makenkap@gmail.com>
2023-09-06 11:09:37 -0400

    configure typeorm data-sources & init migrations

---
+++ src/database/secondary/secondary.data-source.ts
@@ -0,0 +1,13 @@
+import { DataSource } from "typeorm";
+
+export const SecondaryDataSource = new DataSource({
+    type: 'mongodb',
+    host: 'localhost',
+    port: +1,
+    database: 'test',
+    username: 'test',
+    password: 'test',
+    entities: [],
+    synchronize: true,
+    migrations: ['./migration/*{.ts,.js}']
+})
